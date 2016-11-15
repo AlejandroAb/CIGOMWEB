9b720c8f524b63de60973bce7b684cac8b5a11df
Alejandro <Alejandro@10.0.6.104>
2016-11-15 13:12:57 -0600

    First checkin

---
+++ src/java/bobjects/AAA.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bobjects;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class AAA {
+    
+}
